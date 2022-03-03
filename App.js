040d2f3486d3a9a6a81ea8c1bc1959ed2ffa2ce5
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-03 15:31:08 -0300

    commit trocando cor e finalizando UI

---
+++ App.js
@@ -0,0 +1,66 @@
+
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Olá mundo!</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor 1</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor 2</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.botao}>
+            <Text style={styles.textoBotao}>Somar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textoBloco:{
+    fontSize:30,
+    color: '#39b1b8',
+  },
+  texto:{
+    color:'#000',
+    fontSize:30
+  },
+  input:{
+    borderColor:'#000',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+  },
+  bloco:{
+    width:'100%',
+    alignItems:'center',
+    marginTop:30
+  },
+  botao:{
+    backgroundColor:'#39b1b8',
+    width:'80%',
+    textAlign:'center'
+  },
+  textoBotao:{
+    color:"#fff",
+    fontSize:30
+  }
+});
